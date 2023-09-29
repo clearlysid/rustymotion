935af2966d4aad85c8cecb3f0b7dece6b4dfb989
Siddharth <siddharth99c@gmail.com>
2023-09-29 11:29:54 +0530

    fix: correctly uses event loop

--- src/renderer.rs
+++ src/renderer.rs
@@ -13,6 +13,8 @@ use wry::{
     webview::{ScreenshotRegion, WebView, WebViewBuilder},
 };
 
+use crate::ffmpeg;
+
 pub struct RenderOptions {
     pub bundle: String,
     pub output: String,
@@ -22,10 +24,9 @@ pub struct RenderOptions {
     pub muted: Option<bool>,
 }
 
-const FRAMES: u32 = 30;
-
 enum UserEvent {
     PageLoaded,
+    FramePainted,
 }
 
 fn save_frame_to_file(webview: &WebView, frame: u32) {
@@ -35,7 +36,7 @@ fn save_frame_to_file(webview: &WebView, frame: u32) {
             move |image: wry::Result<Vec<u8>>| {
                 let filename = format!("frame-{}.png", frame);
                 let image = image.expect("Couldn't get image");
-                println!("image: {:?}", image);
+                // println!("image: {:?}", image);
 
                 let mut file = File::create(filename).expect("Couldn't create the file");
 
@@ -47,23 +48,19 @@ fn save_frame_to_file(webview: &WebView, frame: u32) {
 }
 
 pub fn render(options: RenderOptions) -> wry::Result<()> {
-    // 1. Validate props
+    // 1. Validate props and process bundle
     // options.bundle
     // options.composition
     // options.frames
     // options.props
 
-    // 2. Process the Bundle
     let bundle_path = PathBuf::from(options.bundle);
-
-    let html_content = read_to_string(bundle_path).expect("Failed to read HTML file");
-
-    println!("HTML content: {}", html_content);
+    let _html_content = read_to_string(bundle_path).expect("Failed to read HTML file");
 
     let width = 1920;
     let height = 1080;
 
-    // 2. Create Webview
+    // 2. Create Window and Webview
 
     let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
     let proxy = event_loop.create_proxy();
@@ -75,64 +72,79 @@ pub fn render(options: RenderOptions) -> wry::Result<()> {
     let ipc_handler = move |_: &Window, req: String| {
         if req == "page-loaded" {
             let _ = proxy.send_event(UserEvent::PageLoaded);
+        } else if req == "frame-painted" {
+            let _ = proxy.send_event(UserEvent::FramePainted);
         }
     };
 
-    let _webview = WebViewBuilder::new(window)?
+    let wv = WebViewBuilder::new(window)?
         .with_html(
             r#"
             <html>
             <body style="display:flex;align-items:center;justify-content:center;">
-            <div style="height:200px;width:200px;background:lightsalmon;">YOLO</div>
+            <div id="box">YOLO</div>
+            <style>#box{display:flex;align-items:center;justify-content:center;height:200px;width:200px;background:lightsalmon;font-family:sans-serif;font-weight:bold;font-size:3rem;}</style>
             </body>
             </html>
         "#,
         )?
-        .with_initialization_script(
-            r#"
-                (function () {
-                    window.addEventListener('DOMContentLoaded', (event) => {
-                        window.ipc.postMessage('page-loaded');
-                    });
-                })();
-                "#,
-        )
+        .with_initialization_script(r#"
+        (function () {
+            window.addEventListener('DOMContentLoaded', (event) => {
+                window.ipc.postMessage('page-loaded');
+            });
+        })();
+        "#,)
         .with_ipc_handler(ipc_handler)
         .build()?;
 
     // 3. Set up Event Loop
+    let mut current_frame = 0;
+    let max_frames = 120;
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::NewEvents(StartCause::Init) => {
-                println!("Wry has started!");
-
-            },
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
             Event::UserEvent(UserEvent::PageLoaded) => {
                 println!("Page loaded!");
 
-                for frame in 0..FRAMES {
-                    // Save frame to file
-                    println!("Saving frame {}", frame);
-                    let set_frame_command =
-                        format!(r#"
-                        document.body.innerHTML = '<div style="height:200px;width:200px;background:lightsalmon;transform:rotate({}deg);">YOLO</div>'
-                        "#, frame * 2);
-                        // format!("remotion_setFrame({})", frame);
+                // Kick off frame rendering
+                wv.evaluate_script(r#"window.ipc.postMessage('frame-painted');"#)
+                    .expect("Failed to send message");
+            }
+            Event::UserEvent(UserEvent::FramePainted) => {
+                if current_frame == max_frames {
+                    // Exit the loop
+                    println!("All frames painted");
 
-                    _webview
-                        .evaluate_script(set_frame_command.as_str())
-                        .unwrap();
+                    // Converting to video
+                    ffmpeg::render_screenshots_to_video("test.mp4".into()).unwrap();
 
-                    save_frame_to_file(&_webview, frame);                    
+                    *control_flow = ControlFlow::Exit;
+                    return;
                 }
 
-                // sleep(Duration::from_millis(1000));
-                // ffmpeg::render_screenshots_to_video("test.mp4".into()).unwrap();
+                println!("Frame {} painted!", current_frame);
 
-                // exit the app
-                // *control_flow = ControlFlow::Exit;
+                // Updates contents to current frame
+                let set_frame_command = format!(
+                    r#"
+                     document.getElementById('box').style.transform = 'rotate({}deg)';
+                     "#,
+                    current_frame * 2
+                );
+                // format!("remotion_setFrame({})", frame);
+                wv.evaluate_script(set_frame_command.as_str()).unwrap();
+
+                // save frame to file
+                save_frame_to_file(&wv, current_frame);
+
+                // advance frame
+                current_frame += 1;
+                wv.evaluate_script(r#"window.ipc.postMessage('frame-painted');"#)
+                    .expect("Failed to send message");
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
