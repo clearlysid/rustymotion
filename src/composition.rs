192439fa85258235e3814c5f919fd77fbecb18f4
Siddharth <siddharth99c@gmail.com>
2023-11-07 16:15:38 +0530

    refactor: fixes rust vs js casing

--- src/composition.rs
+++ src/composition.rs
@@ -1,15 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-// TODO: need to add some flags here to convert JSON camelCase props to Rust snake_case
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Composition {
     pub width: u32,
     pub height: u32,
     pub fps: u32,
-    pub durationInFrames: u32,
     pub id: String,
-    pub serializedResolvedPropsWithCustomSchema: String,
-    pub serializedDefaultPropsWithCustomSchema: String,
+
+    #[serde(rename = "durationInFrames")]
+    pub duration_in_frames: u32,
+    #[serde(rename = "serializedResolvedPropsWithCustomSchema")]
+    pub serialized_resolved_props_with_custom_schema: String,
+    #[serde(rename = "serializedDefaultPropsWithCustomSchema")]
+    pub serialized_default_props_with_custom_schema: String,
 }
 
 pub fn derive(json: String) -> Vec<Composition> {
