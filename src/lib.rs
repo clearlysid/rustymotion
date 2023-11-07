6e19ae5ebfce1e5790088020e30cad8e520245c7
Siddharth <siddharth99c@gmail.com>
2023-11-07 16:16:14 +0530

    refactor: cleans up code and file referencing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use headless_chrome::protocol::cdp::Page;
+use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png;
 use headless_chrome::{Browser, LaunchOptions};
 use std::error::Error;
 use std::fs;
@@ -7,19 +7,13 @@ use std::path::{Path, PathBuf};
 mod composition;
 mod ffmpeg;
 
+#[derive(Debug)]
 pub struct RenderOptions {
     pub bundle: String,
     pub output: String,
     pub composition: String,
     pub props: Option<String>,
-    pub frames: Option<String>,
-}
-
-pub enum UserEvent {
-    PageLoaded,
-    FrameLoaded,
-    FramesComplete,
-    GetCompositions(String),
+    pub frames: Option<(u32, u32)>,
 }
 
 fn unescape_json_string(escaped_json: &str) -> Result<String, serde_json::Error> {
@@ -30,68 +24,71 @@ fn unescape_json_string(escaped_json: &str) -> Result<String, serde_json::Error>
 }
 
 pub fn render(options: RenderOptions) -> Result<(), Box<dyn Error>> {
-    // 1. Validate: bundle, composition, frames, props
-    println!("Rendering with options: {:?}", options.bundle);
+    // 1. Validate bundle and composition, deconstruct frames and options
+    println!("Rendering with options: {:?}", options);
     let bundle_path = PathBuf::from(&options.bundle)
         .canonicalize()
-        .expect("Failed to find bundle");
+        .expect("Bundle folder does not exist.");
+
     let output_file = options.output;
     let composition = options.composition;
-    let frame_start = 0;
-    let frame_end = 30;
+    let frame_start = options.frames.unwrap_or((0, 0)).0;
+    let frame_end = options.frames.unwrap_or((0, 0)).1;
 
     // 2. Create Browser and tab
     let browser = Browser::new(LaunchOptions {
         headless: true,
         enable_gpu: true,
-        window_size: Some((1920, 1080)),
+        window_size: Some((1080, 1080)),
         ..Default::default()
-    })
-    .expect("failed to launch browser");
-    let tab = browser.new_tab().expect("Failed to create tab");
+    })?;
+    let tab = browser.new_tab()?;
 
-    // load the contents of index.html and navigate there
-    let html_file_url = format!("file://{}", bundle_path.to_str().unwrap());
-    tab.navigate_to(&html_file_url)?;
+    // 3. Load HTML and JS
+    let bundle_index_path = bundle_path.join("index.html");
+    let bundle_index_str = bundle_index_path.to_str().unwrap();
+    let bundle_index_url = format!("file://{}", bundle_index_str);
 
-    // load and evaluate the javascript
-    // TODO: get js snippet from bundle dynamically
-    let js = fs::read_to_string("./bundle/bundle.js").expect("Failed to read index.js");
-    tab.evaluate(js.as_str(), false).expect("couldn't run js");
+    tab.navigate_to(&bundle_index_url)?;
 
-    // Get data about the Remotion bundle
+    let bundle_js_path = bundle_path.join("bundle.js");
+    let bundle_js_str = fs::read_to_string(bundle_js_path)?;
+
+    tab.evaluate(&bundle_js_str, true)?;
+
+    // 4. Get data about the Remotion bundle
     tab.evaluate(
         "window.remotion_setBundleMode({ type: 'evaluation' });",
         false,
-    )
-    .expect("couldn't set bundlemode to evaluation");
+    )?;
 
-    // TODO: figure out waiting mechanism.
-    // wait for 2 seconds
-    // Wait::with_sleep(std::time::Duration::from_secs(2));
-
-    let compositions = tab
+    // 5. Get composition data
+    let comps_raw = tab
         .evaluate(
             "window.getStaticCompositions().then(comps => JSON.stringify(comps))",
             true,
-        )
-        .expect("couldn't get comps")
+        )?
         .value
         .unwrap()
         .to_string();
 
-    let escaped_comps = unescape_json_string(&compositions).expect("couldn't unescape comps");
-    let comps = composition::derive(escaped_comps);
+    let comps_sanitized = unescape_json_string(&comps_raw)?;
+    let comps = composition::derive(comps_sanitized);
 
     let comp = comps
         .iter()
         .find(|c| c.id == composition)
         .expect("No matching Composition found");
 
-    // 3. Set up Event Loop
-    let mut frame_current = frame_start;
-    let mut frame_duration = frame_end;
-    let mut fps = 30;
+    // 6. Set up Frame Loop
+    let fps = comp.fps;
+    let frame_duration = match frame_end {
+        0 => comp.duration_in_frames,
+        _ => frame_end,
+    };
+
+    // let width = comp.width;
+    // let height = comp.height;
 
     // TODO: write this into a temp directory
     let frame_dir = Path::new("./frames");
@@ -99,16 +96,8 @@ pub fn render(options: RenderOptions) -> Result<(), Box<dyn Error>> {
         fs::create_dir(frame_dir).expect("Failed to create frame directory");
     }
 
-    // TODO: figure out how to apply these
-    let width = comp.width;
-    let height = comp.height;
-
-    // Update frame duration and fps for the event loop
-    frame_duration = comp.durationInFrames;
-    fps = comp.fps;
-
-    // Prepare composition for rendering
-    let composition_prep_script = format!(
+    // 7. Prepare composition for rendering
+    let comp_prep_script = format!(
         "window.remotion_setBundleMode({{
             type: 'composition',
             compositionName: '{}',
@@ -119,40 +108,38 @@ pub fn render(options: RenderOptions) -> Result<(), Box<dyn Error>> {
             compositionWidth: {},
         }});",
         comp.id,
-        comp.serializedDefaultPropsWithCustomSchema,
-        comp.durationInFrames,
+        comp.serialized_default_props_with_custom_schema,
+        comp.duration_in_frames,
         comp.fps,
         comp.height,
         comp.width
     );
-
-    tab.evaluate(&composition_prep_script, true)
-        .expect("couldn't set composition");
+    tab.evaluate(&comp_prep_script, true)?;
 
     // sleep for 2 seconds main thread
     std::thread::sleep(std::time::Duration::from_secs(1));
 
-    // 4. Capture Frames
-    for frame in 1..frame_duration {
+    // 8. Capture Frames
+    for frame in frame_start..frame_duration {
         println!("Writing Frame: {}", frame);
 
         // Updates bundle app to current frame
-        let set_frame_command = format!("window.remotion_setFrame({}, '{}');", frame, composition);
-        tab.evaluate(set_frame_command.as_str(), true)
-            .expect("couldn't set frame");
+        let set_frame_script = format!("window.remotion_setFrame({}, '{}');", frame, composition);
+        tab.evaluate(&set_frame_script.as_str(), true)?;
 
         // take screenshot
-        let png_data =
-            tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Png, None, None, true)?;
-        let name = format!("frame-{}.png", frame);
-        let path = frame_dir.join(name);
-        std::fs::write(path, png_data)?;
+        let png_data = tab.capture_screenshot(Png, None, None, true)?;
+        println!("PNG Data: {:?}", &png_data[0..5]);
+
+        // let name = format!("frame-{}.png", frame);
+        // let path = frame_dir.join(name);
+        // std::fs::write(path, png_data)?;
     }
 
     println!("Frames captured.");
     tab.close(true).expect("Failed to close tab");
 
-    // 5. Encode into video
+    // 9. Encode into video
     ffmpeg::encode_video(&output_file, fps, frame_dir)?;
     println!("Video encoded.");
     return Ok(());
